dad74337814f89092c283948e1f521b842b0d1c8
MoonRainy21 <shmoon323@gmail.com>
2023-10-30 10:07:21 +0900

    Apply review

--- helpers/checkSolution.ts
+++ helpers/checkSolution.ts
@@ -5,14 +5,14 @@ const checkStateConnection = (
   if (firstState.endFrame !== secondState.startFrame) {
     new Error(`State ${firstState} and ${secondState} are not connected!`);
   }
-  firstState.activeEntities.map((entityId) => {
-    if (!secondState.activeEntities.includes(entityId)) new Error(`Entity ${entityId} is missing from the state ${secondState}!`);
+  firstState.activeEntities.forEach((entityId) => {
+    if (!secondState.activeEntities.includes(entityId)) throw Error(`Entity ${entityId} is missing from the state ${secondState}!`);
     const firstPath = firstState.paths.find((path) => path.eid === entityId);
     const secondPath = secondState.paths.find((path) => path.eid === entityId);
-    if (!firstPath) new Error(`Entity ${entityId} doesn't have path at the state ${firstState}!`);
-    if (!secondPath) new Error(`Entity ${entityId} doesn't have path at the state ${secondState}!`);
+    if (!firstPath) throw Error(`Entity ${entityId} doesn't have path at the state ${firstState}!`);
+    if (!secondPath) throw Error(`Entity ${entityId} doesn't have path at the state ${secondState}!`);
     if (firstPath?.path(firstState.endFrame) !== secondPath?.path(secondState.startFrame)) {
-      new Error(`Entity ${entityId} doesn't have the same path at the state ${firstState} and ${secondState}!`);
+      throw Error(`Entity ${entityId} doesn't have the same path at the state ${firstState} and ${secondState}!`);
       //요기서 return false 어떻게함??
     }
   });
